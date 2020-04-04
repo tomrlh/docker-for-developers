6e281e99be9945b1b524f9d6004f838771c170aa
Thiago Medeiros <si-thiago@hotmail.com>
2020-04-04 16:11:37 -0300

    CRUD app working with backend(express)/frontend(nginx)/database(mongo) in differents containers :whale: (Docker is awesome)

--- node-mongo-compose/backend/app.js
+++ node-mongo-compose/backend/app.js
@@ -2,12 +2,29 @@ const express = require('express')
 const restful = require('node-restful')
 const mongoose = restful.mongoose
 const server = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // Database
 mongoose.Promise = global.Promise // mongoose to use Promises api from node, because mongoose.Promise is deprecated
 mongoose.connect('mongodb://db/mydb')
 
-server.get('/', (req, res, next) => res.send('Backend'))
+// Middlewares
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.json())
+server.use(cors())
+
+// ODM
+const Client = restful.model('Client', {
+    name: {type: String, required: true}
+})
+
+// REST API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true})
+
+// Routes
+Client.register(server, '/clients')
 
 // Start Server
 server.listen(3000)
