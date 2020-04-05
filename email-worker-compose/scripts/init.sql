b41e4600aff657c85bebf0da7589c7ce0bbfcca6
Thiago Medeiros <si-thiago@hotmail.com>
2020-04-05 01:27:30 -0300

    Starting the final project

---
+++ email-worker-compose/scripts/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE email_sender;
+
+\c email_sender
+
+CREATE TABLE emails (
+    id SERIAL NOT NULL,
+    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    subject VARCHAR(100) NOT NULL,
+    message VARCHAR(250) NOT NULL
+)
